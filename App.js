8a30b971fb499cd7543a5d17502e1c662f91495e
abisekhsubedi <abisekh.work@gmail.com>
2022-07-10 22:28:27 +0545

    feat: Select image functionality

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 const gradientPngUri =
   "https://images.unsplash.com/photo-1604079628040-94301bb21b91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  const openImagePickerAsync = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) return;
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Image source={{ uri: gradientPngUri }} style={styles.gradient} />
       <Text style={styles.text}>This is not my gradient.</Text>
       <StatusBar style="auto" />
-      <TouchableOpacity
-      onPress={()=> alert("Hello, hero!")}
-      style={styles.button}
-      >
-        <Text style={styles.buttonTxt}> Revert your life</Text>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonTxt}> select photo</Text>
       </TouchableOpacity>
     </View>
   );
@@ -43,10 +69,17 @@ const styles = StyleSheet.create({
     paddingTop: 12,
     paddingBottom: 12,
     paddingLeft: 24,
-    paddingRight: 24
+    paddingRight: 24,
   },
   buttonTxt: {
     fontSize: 18,
     color: "#444",
- }
+  },
+  thumbnail: {
+    width: 200,
+    height: 200,
+    marginBottom: 40,
+    borderRadius: 12,
+    resizeMode: "contain",
+  },
 });
