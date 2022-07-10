2492dcaa20b74dae0a6bee11b49ff916fe1fa7b5
abisekhsubedi <abisekh.work@gmail.com>
2022-07-10 20:54:29 +0545

    feat: Add gradient imaga & title

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+const gradientPngUri =
+  "https://images.unsplash.com/photo-1604079628040-94301bb21b91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{ uri: gradientPngUri }}
+        style={styles.gradient}
+      />
+      <Text style={styles.text}>This is not my gradient.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+  color: "#555",
+  fontSize: 18,
+  fontWeight: "normal"
+ },
+ gradient: {
+    width: 200,
+    height: 200,
+    marginBottom: 40,
+    borderRadius: 12
+ }
 });
